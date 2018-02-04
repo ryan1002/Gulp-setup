6701ef1e65b11b9e93248a8ef612ca9c7e8fe327
Ryan Modeste <ryanmodeste@Ryans-MacBook-Pro.local>
2018-02-04 17:31:05 +0000

    autoprefixer installed and working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
 var browserSync = require('browser-sync').create();
 
 gulp.task('browserSync', function() {
@@ -11,19 +13,23 @@ gulp.task('browserSync', function() {
 })
 
 gulp.task('sass', function () {
-	return gulp.src('./src/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-	  .pipe(gulp.dest('./src/css/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return gulp.src('./src/sass/*.scss')
+  .pipe(sass({
+     outputStyle: 'expanded'
+  })
+  .on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./src/css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // copy all html files
 
 gulp.task('copyHtml', function(){
   gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
